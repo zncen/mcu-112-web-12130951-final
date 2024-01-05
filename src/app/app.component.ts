7b84486c2628515c0ab1a8e490743a479169388d
zncen <1581623769Jijiorg??>
2024-01-05 15:51:26 +0800

    將header的導覽頁移動至nav-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [NavBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
