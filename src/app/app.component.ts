fcd715d917193bfcf4a237b5a0eaf28101d06306
zncen <1581623769Jijiorg??>
2024-01-05 16:51:25 +0800

    把網頁內距改好還有綠色背景跟conduit字體陰影

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
