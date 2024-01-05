8afdfb62dc449d287da182b68e3c26c6de0c6f20
zncen <1581623769Jijiorg??>
2024-01-06 02:22:30 +0800

    建立頁腳

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent,HeaderComponent],
+  imports: [NavBarComponent,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
