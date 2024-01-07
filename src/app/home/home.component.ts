ce5b0751729b0a9826ea594eda5f4cbe86b15aaf
zncen <1581623769Jijiorg??>
2024-01-08 00:23:50 +0800

    製作首頁內容container容器

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { ContainerComponent } from '../container/container.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,ContainerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
