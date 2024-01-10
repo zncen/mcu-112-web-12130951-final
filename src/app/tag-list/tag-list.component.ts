b7e543f66421a6b27189772eb4d9a383382532bc
zncen <1581623769Jijiorg??>
2024-01-10 23:20:07 +0800

    feat(tag-list):標籤元件樣式實作

--- src/app/tag-list/tag-list.component.ts
+++ src/app/tag-list/tag-list.component.ts
@@ -1,13 +1,15 @@
 import { Component, HostBinding } from '@angular/core';
+import { TagComponent } from '../tag/tag.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tag-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, TagComponent],
   templateUrl: './tag-list.component.html',
-  styleUrl: './tag-list.component.css'
+  styleUrl: './tag-list.component.css',
 })
 export class TagListComponent {
-  
-  @HostBinding('class') class='tag-list';
+  @HostBinding('class') class = 'tag-list';
+  tags = ['enim', 'repellat', 'est', 'eos'];
 }
---
+++ src/app/tag/tag.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  standalone: true,
+  imports: [],
+  templateUrl: './tag.component.html',
+  styleUrl: './tag.component.css',
+})
+export class TagComponent {
+  @Input({ required: true })
+  name!: string;
+}
