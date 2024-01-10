e4aaec970ced96d76b7e5a6b67af0ad2df22382b
zncen <1581623769Jijiorg??>
2024-01-10 22:46:39 +0800

    feat(tag-list):標籤清單元件實作

---
+++ src/app/tag-list/tag-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tag-list.component.html',
+  styleUrl: './tag-list.component.css'
+})
+export class TagListComponent {
+
+}
