ce931d075e97bee5bab25fcfc41ae0792b8542de
zncen <1581623769Jijiorg??>
2024-01-12 03:28:03 +0800

    feat(article-form):新文章表單頁面切換實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { ArticleFormPageComponent } from './article-form-page/article-form-page.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'article/form', component: ArticleFormPageComponent },
 ];
